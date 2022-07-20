6559410308dcd713318ff91e75e8548ef2bfe4f8
Rabbi Khan <rabbikhan11002299@gmail.com>
2022-07-20 20:40:02 +0600

    Fixed Some Issue

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const dev = {
     app: {
-        port: process.env.PORT || 5000
+        port: process.env.PORT || 3000
     },
     db: {
         url: process.env.DB_URL || "mongodb://localhost:27017/UserDemoDB"
